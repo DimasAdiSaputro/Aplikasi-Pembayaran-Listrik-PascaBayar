41a4571d3b15bf9c55ecc8f45b9350b0af5cb35a
DimasAdiSaputro <dimasadisaputr0699@gmail.com>
2024-01-20 18:04:44 +0700

    update SQL

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -3,7 +3,7 @@
 		<div class="row text-muted">
 			<div class="col-6 text-start">
 				<p class="mb-0">
-					<a class="text-muted" href="https://github.com/MuhammadAkbar11" target="_blank"><strong>MuhammadAkbar11</strong></a> &copy;
+					<a class="text-muted" href="https://github.com/DimasAdiSaputro" target="_blank"><strong>Dimas Adi Saputro</strong></a> &copy;
 				</p>
 			</div>
 
